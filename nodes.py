9442ebc2bb7b1b48a9efa8b0098d2c5513e338f9
alexfaker <1396657985@qq.com>
2025-07-05 14:10:25 +0800

    更新图片上传节点

--- nodes.py
+++ nodes.py
@@ -368,7 +368,7 @@ class AVLoadImageFromURL:
                 try:
                     headers = {
                         'User-Agent': user_agent,
-                        'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
                         'Accept-Encoding': 'gzip, deflate, br',
                         'DNT': '1',
